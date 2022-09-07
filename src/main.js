918e2e0a02828b96fffe765d496de2b30984273c
LightningYTlol <izayikhan@gmail.com>
2022-09-07 16:38:37 -0700

    change from Vue to React

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import './assets/main.css';
-import Home from "./views/Home.vue";
-import router from './router'
-
-const app = createApp(App)
-    .use(router)
-    .mount('#app');
